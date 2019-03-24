31aa1990ebfa7857bb1899882e6942105eac33be
Olivia Ea <oliviaea@olivias-mbp.attlocal.net>
2019-03-23 18:11:22 -0700

    Finished count_recursively.py

---
+++ anagramofpalindrome.py
@@ -0,0 +1,33 @@
+def is_anagram_of_palindrome(word):
+    """Is the word an anagram of a palindrome?
+
+    >>> is_anagram_of_palindrome("a")
+    True
+
+    >>> is_anagram_of_palindrome("ab")
+    False
+
+    >>> is_anagram_of_palindrome("aab")
+    True
+
+    >>> is_anagram_of_palindrome("arceace")
+    True
+
+    >>> is_anagram_of_palindrome("arceaceb")
+    False
+
+    """
+
+    for letter in word:
+
+
+
+
+if __name__ == "__main__":
+    import doctest
+
+    print()
+    result = doctest.testmod()
+    if not result.failed:
+        print("ALL TESTS PASSED. GOOD WORK!")
+    print()
---
+++ count_recursively.py
@@ -0,0 +1,25 @@
+
+def count_recursively(lst):
+    """Return number of items in a list, using recursion.
+
+    >>> count_recursively([])
+    0
+
+    >>> count_recursively([1, 2, 3])
+    3
+    """
+
+    if lst == []:
+        return 0
+    else:
+        lst.pop()
+        return 1 + count_recursively(lst)
+
+if __name__ == "__main__":
+    import doctest
+
+    print()
+    result = doctest.testmod()
+    if not result.failed:
+        print("ALL TESTS PASSED. GOOD WORK!")
+    print()
