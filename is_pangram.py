435df4d8c415f1e7eff8c43a5f9bd45da917d3ba
Olivia Ea <oliviaea@olivias-mbp.attlocal.net>
2019-03-23 18:35:28 -0700

    Finished is_panagram

---
+++ is_pangram.py
@@ -0,0 +1,28 @@
+def is_pangram(sentence):
+    """Given a string, return True if it is a pangram, False otherwise.
+
+    >>> is_pangram("The quick brown fox jumps over the lazy dog!")
+    True
+
+    >>> is_pangram("I like cats, but not mice")
+    False
+    """
+
+    alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
+    new_sentence = sentence.lower
+    sentence_set = set(sentence)
+
+    if alphabet.issubset(sentence_set):
+        return True
+    else:
+        return False  
+
+if __name__ == "__main__":
+    import doctest
+
+    print()
+    result = doctest.testmod()
+    if not result.failed:
+        print("ALL TESTS PASSED. GOOD WORK!")
+    print()
