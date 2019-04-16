3ca5d34ef66f26949934fd8f16944430bbf4a462
Olivia Ea <ea.olivia1@gmail.com>
2019-04-16 16:17:42 -0700

    Finished anagram problem

--- anagramofpalindrome.py
+++ anagramofpalindrome.py
@@ -1,33 +1,37 @@
+import unittest
+
 def is_anagram_of_palindrome(word):
-    """Is the word an anagram of a palindrome?
+    """Is the word an anagram of a palindrome?"""
 
-    >>> is_anagram_of_palindrome("a")
-    True
-
-    >>> is_anagram_of_palindrome("ab")
-    False
-
-    >>> is_anagram_of_palindrome("aab")
-    True
-
-    >>> is_anagram_of_palindrome("arceace")
-    True
-
-    >>> is_anagram_of_palindrome("arceaceb")
-    False
-
-    """
+    letter_dict = {}
+    # create empty dict to keep track of all unique letters
 
     for letter in word:
+        letter_dict[letter] = letter_dict.get(letter, 0) + 1
 
+    seen = False
 
+    for count in letter_dict.values():
+        if count % 2 != 0:  # look at all letter values that are odd
+            if seen:    # only executes if TRUE; seen default is FALSE
+                return False    
+            seen = True
+    return True
+
+class Test(unittest.TestCase):
+    data = [
+        ("a", True),
+        ("ab", False),
+        ("aab", True), 
+        ("arceace", True),
+        ("arceaceb", False)
+    ]
+
+    def test_is_anagram_of_palindrome(self):
+        for [word, expected] in self.data:
+            result = is_anagram_of_palindrome(word)
+            self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
-    import doctest
-
-    print()
-    result = doctest.testmod()
-    if not result.failed:
-        print("ALL TESTS PASSED. GOOD WORK!")
-    print()
+    unittest.main()
