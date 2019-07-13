cd90dc5a65323568d4d8bbe6380838ae89aab3f4
Olivia Ea <ea.olivia1@gmail.com>
2019-07-13 12:24:31 -0700

    Minor edits

--- change_duplicate_word.py
+++ change_duplicate_word.py
@@ -1,10 +1,9 @@
 import unittest
 from random import shuffle
 
-def scramble_word(word):
-
+def reverse_word(word):
     split_word = list(word)
-    shuffle(split_word)
+    split_word.reverse()
 
     return ''.join(split_word)
 
@@ -16,7 +15,6 @@ def change_duplicate_word(phrase):
         if word not in track_words:
             track_words.append(word)
         else:
-            new_word = scramble_word(word)
+            new_word = split_word(word)
             track_words.append(new_word)
-            
     return ' '.join(track_words)
